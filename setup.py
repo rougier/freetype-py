fbb48d046a3ac835e0bcdaffc1fcb72ec7a8b80a
Nicolas.Rougier <Nicolas.Rougier@27b6269b-3101-353a-0618-6af1094ad73e>
2011-01-30 22:07:49 +0000

    bugfix: fix metadata

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@
 # -----------------------------------------------------------------------------
 from distutils.core import setup
 
-setup( name        = 'Freetype',
+setup( name        = 'freetype-py',
        version     = '0.1',
        description = 'Freetype python bindings',
        author      = 'Nicolas P. Rougier',
        author_email='Nicolas.Rougier@inria.fr',
-       #      url='http://www.python.org/sigs/distutils-sig/',
+       url='http://code.google.com/p/freetype-py/',
        packages=['freetype'],
        classifiers = [
           'Development Status :: 3 - Alpha',
           'Environment :: X11 Applications',
           'Environment :: MacOS X',
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: GNU General Public License (GPL)'
+          'License :: OSI Approved :: GNU General Public License (GPL)',
           'Operating System :: MacOS',
           'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX',
