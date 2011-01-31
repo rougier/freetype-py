028b41c6fbb0dc2bc9d7ff9b6338e684f53b5f41
Nicolas.Rougier <Nicolas.Rougier@27b6269b-3101-353a-0618-6af1094ad73e>
2011-01-31 08:30:18 +0000

    feature: increased version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from distutils.core import setup
 
 setup( name        = 'freetype-py',
-       version     = '0.1',
+       version     = '0.1.2',
        description = 'Freetype python bindings',
        author      = 'Nicolas P. Rougier',
        author_email='Nicolas.Rougier@inria.fr',
