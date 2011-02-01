f64606a8688ae2b1ebc4bbfbbdc352118ce075f0
Nicolas.Rougier <Nicolas.Rougier@27b6269b-3101-353a-0618-6af1094ad73e>
2011-02-01 21:09:04 +0000

    bugfix: removed uneeded bits function

--- examples/glyph-alpha.py
+++ examples/glyph-alpha.py
@@ -9,13 +9,6 @@ Glyph bitmap monochrome rendring
 '''
 from freetype import *
 
-def bits(x):
-    data = []
-    for i in range(8):
-        data.insert(0, int((x & 1) == 1))
-        x = x >> 1
-    return data
-
 if __name__ == '__main__':
     import numpy
     import matplotlib.pyplot as plt
