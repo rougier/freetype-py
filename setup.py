acc613a244d01f83de11865eb1bf532857ef33e2
Nicolas.Rougier <Nicolas.Rougier@27b6269b-3101-353a-0618-6af1094ad73e>
2011-03-15 21:33:54 +0000

    Updated version micro number and notes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from distutils.core import setup
 
 setup( name        = 'freetype-py',
-       version     = '0.3.0',
+       version     = '0.3.1',
        description = 'Freetype python bindings',
        author      = 'Nicolas P. Rougier',
        author_email='Nicolas.Rougier@inria.fr',
