a3df613cc41058027ed8fe83580a45f2d1e55b38
Nicolas.Rougier <Nicolas.Rougier@27b6269b-3101-353a-0618-6af1094ad73e>
2011-04-01 10:41:08 +0000

    Added color example

--- examples/glyph-outline.py
+++ examples/glyph-outline.py
@@ -14,26 +14,23 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     face = Face('./Vera.ttf')
-    face.set_char_size( 96*64 )
-
+    face.set_char_size( 4*48*64 )
     flags = FT_LOAD_DEFAULT | FT_LOAD_NO_BITMAP
     face.load_char('S', flags )
     slot = face.glyph
     glyph = slot.get_glyph()
     stroker = Stroker( )
-    stroker.set(32, FT_STROKER_LINECAP_ROUND, FT_STROKER_LINEJOIN_ROUND, 0 )
+    stroker.set(64, FT_STROKER_LINECAP_ROUND, FT_STROKER_LINEJOIN_ROUND, 0 )
     glyph.stroke( stroker )
     blyph = glyph.to_bitmap(FT_RENDER_MODE_NORMAL, Vector(0,0)) 
-
     bitmap = blyph.bitmap
-    width  = bitmap.width
-    rows   = bitmap.rows
-    pitch  = bitmap.pitch
-
+    width, rows, pitch = bitmap.width, bitmap.rows, bitmap.pitch
+    top, left = blyph.top, blyph.left
     data = []
     for i in range(rows):
         data.extend(bitmap.buffer[i*pitch:i*pitch+width])
     Z = numpy.array(data,dtype=numpy.ubyte).reshape(rows, width)
+    plt.figure(figsize=(6,8))
     plt.imshow(Z, interpolation='nearest', cmap=plt.cm.gray_r)
     plt.show()
     
