709bc71a62ed18864f345f9d1e78920d54f1c7b6
Matthew Sitton <matthewsitton@gmail.com>
2015-01-25 10:48:11 -0600

    Fix for FT_Done_FreeType never being called.

--- freetype/__init__.py
+++ freetype/__init__.py
@@ -29,34 +29,44 @@ __handle__ = None
 
 FT_Library_filename = filename
 
+class _FT_Library_Wrapper(FT_Library):
+    '''Subclass of FT_Library to help with calling FT_Done_FreeType'''
+    # for some reason this doesn't get carried over and ctypes complains
+    _type_ = FT_Library._type_
+
+    # Store ref to FT_Done_FreeType otherwise it will be deleted before needed.
+    _ft_done_freetype = FT_Done_FreeType
+
+    def __del__(self):
+        # call FT_Done_FreeType
+        self._ft_done_freetype(self)
+
+def _init_freetype():
+    global __handle__
+
+    __handle__ = _FT_Library_Wrapper()
+    error = FT_Init_FreeType( byref(__handle__) )
+
+    if error: raise FT_Exception(error)
+
+    try:
+        set_lcd_filter( FT_LCD_FILTER_DEFAULT )
+    except:
+        pass
+
 # -----------------------------------------------------------------------------
 # High-level API of FreeType 2
 # -----------------------------------------------------------------------------
 
-def __del_library__(self):
-    global __handle__
-    if __handle__:
-        try:
-            FT_Done_FreeType(self)
-            __handle__ = None
-        except:
-            pass
-FT_Library.__del__ = __del_library__
 
 def get_handle():
     '''
     Get unique FT_Library handle
     '''
-    global __handle__
+
     if not __handle__:
-        __handle__ = FT_Library( )
-        error = FT_Init_FreeType( byref(__handle__) )
-        if error: raise FT_Exception(error)
-        try:
-            set_lcd_filter( FT_LCD_FILTER_DEFAULT )
-        except:
-            pass
-        if error: raise FT_Exception(error)
+        _init_freetype()
+
     return __handle__
 
 def version():
