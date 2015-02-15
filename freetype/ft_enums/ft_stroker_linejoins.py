fc08567d34c87a1165ddb1d5cf36191e235d503e
Matthew Sitton <matthewsitton@gmail.com>
2015-02-15 09:23:34 -0600

    Fix whitespace issue.

--- freetype/ft_enums/ft_stroker_linejoins.py
+++ freetype/ft_enums/ft_stroker_linejoins.py
@@ -30,7 +30,7 @@ FT_STROKER_LINEJOIN_MITER
 FT_STROKER_LINEJOINS = { 'FT_STROKER_LINEJOIN_ROUND': 0,
                          'FT_STROKER_LINEJOIN_BEVEL': 1,
                          'FT_STROKER_LINEJOIN_MITER_VARIABLE': 2,
-    					 'FT_STROKER_LINEJOIN_MITER': 2,
+                         'FT_STROKER_LINEJOIN_MITER': 2,
                          'FT_STROKER_LINEJOIN_MITER_FIXED': 3}
 
 globals().update(FT_STROKER_LINEJOINS)
