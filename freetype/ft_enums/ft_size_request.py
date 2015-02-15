aaa506fd2cc1d3be69253ecf9f3c14eccbba2dae
Matthew Sitton <matthewsitton@gmail.com>
2015-02-15 05:51:03 -0600

    Implement ft_size_request enums

---
+++ freetype/ft_enums/ft_size_request.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+FT_SIZE_REQUEST = { 'FT_SIZE_REQUEST_TYPE_NOMINAL': 0,
+                    'FT_SIZE_REQUEST_TYPE_REAL_DIM': 1,
+                    'FT_SIZE_REQUEST_TYPE_BBOX': 2,
+                    'FT_SIZE_REQUEST_TYPE_CELL': 3,
+                    'FT_SIZE_REQUEST_TYPE_SCALES': 4}
+
+globals().update(FT_SIZE_REQUEST)
