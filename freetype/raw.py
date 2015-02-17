ff2dd91752c0b3df216fc564947988bd25fb8744
Matthew Sitton <matthewsitton@gmail.com>
2015-02-17 16:07:09 -0600

    Rename _def_func to _ft_func as the function now contains freetype specific code.

--- freetype/raw.py
+++ freetype/raw.py
@@ -78,8 +78,13 @@ class _FTUndefinedFunction(object):
         warnings.warn(message)
         warnings.simplefilter("default")
 
-def _def_func(name, returnType, paramTypes, version=None):
-    '''Define function for library'''
+def _ft_func(name, returnType, paramTypes, version=None):
+    '''
+    Define function for the freetype library
+
+    Version requirements can be set on functions which will
+    emit a warning for older freetype versions.
+    '''
     global _version
 
     if version is not None:
@@ -107,17 +112,17 @@ def _def_func(name, returnType, paramTypes, version=None):
 
 # functions needed to do version checking on the freetype library.
 # Need to be defined before any function requireing a specific version.
-FT_Init_FreeType    = _def_func(
+FT_Init_FreeType    = _ft_func(
         'FT_Init_FreeType',
          FT_Error,
         (POINTER(FT_Library),))
 
-FT_Done_FreeType    = _def_func(
+FT_Done_FreeType    = _ft_func(
         'FT_Done_FreeType',
          FT_Error,
         (FT_Library,))
 
-FT_Library_Version  = _def_func(
+FT_Library_Version  = _ft_func(
         'FT_Library_Version',
          None,
         (FT_Library, POINTER(FT_Int), POINTER(FT_Int), POINTER(FT_Int)))
@@ -149,144 +154,144 @@ FT_Library_Version  = _def_func(
 #end macros
 # FT_Init_FreeType                    # defined earlier for version checking.
 # FT_Done_FreeType                    # defined earlier for version checking.
-FT_New_Face            = _def_func(
+FT_New_Face            = _ft_func(
         'FT_New_Face',
         FT_Error,
         (FT_Library, c_char_p, FT_Long, POINTER(FT_Face)))
 
-FT_New_Memory_Face     = _def_func(
+FT_New_Memory_Face     = _ft_func(
         'FT_New_Memory_Face',
          FT_Error,
         (FT_Library, POINTER(FT_Byte), FT_Long, FT_Long, POINTER(FT_Face)))
 
-FT_Open_Face           = _def_func(
+FT_Open_Face           = _ft_func(
         'FT_Open_Face',
          FT_Error,
         (FT_Library, POINTER(FT_Open_Args), FT_Long, POINTER(FT_Face)))
 
-FT_Attach_File         = _def_func(
+FT_Attach_File         = _ft_func(
         'FT_Attach_File',
         FT_Error,
         (FT_Face, c_char_p))
 
-FT_Attach_Stream       = _def_func(
+FT_Attach_Stream       = _ft_func(
         'FT_Attach_Stream',
         FT_Error,
         (FT_Face, POINTER(FT_Open_Args)))
 
-FT_Reference_Face      = _def_func(
+FT_Reference_Face      = _ft_func(
         'FT_Reference_Face',
         FT_Error,
         (FT_Face,),
          version=(2,4,2))
 
-FT_Done_Face           = _def_func(
+FT_Done_Face           = _ft_func(
         'FT_Done_Face',
         FT_Error,
         (FT_Face,))
 
-FT_Select_Size         = _def_func(
+FT_Select_Size         = _ft_func(
         'FT_Select_Size',
         FT_Error,
         (FT_Face, FT_Int))
 
-FT_Request_Size        = _def_func(
+FT_Request_Size        = _ft_func(
         'FT_Request_Size',
         FT_Error,
         (FT_Face, FT_Size_Request))
 
-FT_Set_Char_Size       = _def_func(
+FT_Set_Char_Size       = _ft_func(
         'FT_Set_Char_Size',
         FT_Error,
         (FT_Face, FT_F26Dot6, FT_F26Dot6, FT_UInt, FT_UInt))
 
-FT_Set_Pixel_Sizes     = _def_func(
+FT_Set_Pixel_Sizes     = _ft_func(
         'FT_Set_Pixel_Sizes',
         FT_Error,
         (FT_Face, FT_UInt, FT_UInt))
 
-FT_Load_Glyph          = _def_func(
+FT_Load_Glyph          = _ft_func(
         'FT_Load_Glyph',
         FT_Error,
         (FT_Face, FT_UInt, FT_Int32))
 
-FT_Load_Char           = _def_func(
+FT_Load_Char           = _ft_func(
         'FT_Load_Char',
         FT_Error,
         (FT_Face, FT_ULong, FT_Int32))
 
-FT_Set_Transform       = _def_func(
+FT_Set_Transform       = _ft_func(
         'FT_Set_Transform',
         None,
         (FT_Face, POINTER(FT_Matrix), POINTER(FT_Vector)))
 
-FT_Render_Glyph        = _def_func(
+FT_Render_Glyph        = _ft_func(
         'FT_Render_Glyph',
         FT_Error,
         (FT_GlyphSlot, FT_Render_Mode))
 
-FT_Get_Kerning         = _def_func(
+FT_Get_Kerning         = _ft_func(
         'FT_Get_Kerning',
         FT_Error,
         (FT_Face, FT_UInt, FT_UInt, FT_UInt, POINTER(FT_Vector)))
 
-FT_Get_Track_Kerning   = _def_func(
+FT_Get_Track_Kerning   = _ft_func(
         'FT_Get_Track_Kerning',
         FT_Error,
         (FT_Face, FT_Fixed, FT_Int, POINTER(FT_Fixed)))
 
-FT_Get_Glyph_Name      = _def_func(
+FT_Get_Glyph_Name      = _ft_func(
         'FT_Get_Glyph_Name',
         FT_Error,
         (FT_Face, FT_UInt, FT_Pointer, FT_UInt))
 
-FT_Get_Postscript_Name = _def_func(
+FT_Get_Postscript_Name = _ft_func(
         'FT_Get_Postscript_Name',
         c_char_p,
         (FT_Face,))
 
-FT_Select_Charmap      = _def_func(
+FT_Select_Charmap      = _ft_func(
         'FT_Select_Charmap',
         FT_Error,
         (FT_Face, FT_Encoding))
 
-FT_Set_Charmap         = _def_func(
+FT_Set_Charmap         = _ft_func(
         'FT_Set_Charmap',
         FT_Error,
         (FT_Face, FT_CharMap))
 
-FT_Get_Charmap_Index   = _def_func(
+FT_Get_Charmap_Index   = _ft_func(
         'FT_Get_Charmap_Index',
         FT_Error,
         (FT_CharMap,))
 
-FT_Get_Char_Index      = _def_func(
+FT_Get_Char_Index      = _ft_func(
         'FT_Get_Char_Index',
         FT_UInt,
         (FT_Face, FT_ULong))
 
-FT_Get_First_Char      = _def_func(
+FT_Get_First_Char      = _ft_func(
         'FT_Get_First_Char',
         FT_ULong,
         (FT_Face, FT_UInt))
 
-FT_Get_Next_Char       = _def_func(
+FT_Get_Next_Char       = _ft_func(
         'FT_Get_Next_Char',
         FT_ULong,
         (FT_Face, FT_ULong, POINTER(FT_UInt)))
 
-FT_Get_Name_Index      = _def_func(
+FT_Get_Name_Index      = _ft_func(
         'FT_Get_Name_Index',
         FT_UInt,
         (FT_Face, POINTER(FT_String)))
 
-FT_Get_SubGlyph_Info   = _def_func(
+FT_Get_SubGlyph_Info   = _ft_func(
         'FT_Get_SubGlyph_Info',
         FT_Error,
         (FT_GlyphSlot, FT_UInt, POINTER(FT_Int), POINTER(FT_UInt),
         POINTER(FT_Int), POINTER(FT_Int), POINTER(FT_Matrix)))
 
-FT_Get_FSType_Flags    = _def_func(
+FT_Get_FSType_Flags    = _ft_func(
         'FT_Get_FSType_Flags',
         FT_UShort,
         (FT_Face,),
@@ -323,7 +328,7 @@ FT_Get_FSType_Flags    = _def_func(
 # FT_Outline_Done_Internal
 # FT_Outline_Check
 
-FT_Outline_Get_CBox         = _def_func(
+FT_Outline_Get_CBox         = _ft_func(
         'FT_Outline_Get_CBox',
         POINTER(FT_Outline),
         (POINTER(FT_BBox),))
@@ -352,12 +357,12 @@ FT_Outline_Get_CBox         = _def_func(
 # FT_Load_Sfnt_Table
 # FT_Sfnt_Table_Info
 
-FT_Get_CMap_Language_ID  = _def_func(
+FT_Get_CMap_Language_ID  = _ft_func(
         'FT_Get_CMap_Language_ID',
         FT_ULong,
         (FT_CharMap,))
 
-FT_Get_CMap_Format       = _def_func(
+FT_Get_CMap_Format       = _ft_func(
         'FT_Get_CMap_Format',
         FT_Long,
         (FT_CharMap,))
@@ -371,7 +376,7 @@ FT_Get_CMap_Format       = _def_func(
 ##### FT_WINFONTS_H #####
 
 ##### FT_GLYPH_H #####
-FT_Get_Glyph           = _def_func(
+FT_Get_Glyph           = _ft_func(
         'FT_Get_Glyph',
         FT_Error,
         (FT_GlyphSlot, POINTER(FT_Glyph)))
@@ -379,17 +384,17 @@ FT_Get_Glyph           = _def_func(
 # FT_Glyph_Copy
 # FT_Glyph_Transform
 
-FT_Glyph_Get_CBox      = _def_func(
+FT_Glyph_Get_CBox      = _ft_func(
         'FT_Glyph_Get_CBox',
         None,
         (FT_Glyph, FT_UInt, POINTER(FT_BBox)))
 
-FT_Glyph_To_Bitmap     = _def_func(
+FT_Glyph_To_Bitmap     = _ft_func(
         'FT_Glyph_To_Bitmap',
         FT_Error,
         (POINTER(FT_Glyph), FT_Render_Mode, POINTER(FT_Vector), FT_Bool))
 
-FT_Done_Glyph          = _def_func(
+FT_Done_Glyph          = _ft_func(
         'FT_Done_Glyph',
         None,
         (FT_Glyph,))
@@ -400,7 +405,7 @@ FT_Done_Glyph          = _def_func(
 ##### FT_BITMAP_H #####
 
 ##### FT_BBOX_H #####
-FT_Outline_Get_BBox         = _def_func(
+FT_Outline_Get_BBox         = _ft_func(
         'FT_Outline_Get_BBox',
         FT_Error,
         (POINTER(FT_Outline), POINTER(FT_BBox)))
@@ -411,12 +416,12 @@ FT_Outline_Get_BBox         = _def_func(
 ##### FT_MULTIPLE_MASTERS_H #####
 
 ##### FT_SFNT_NAMES_H #####
-FT_Get_Sfnt_Name_Count = _def_func(
+FT_Get_Sfnt_Name_Count = _ft_func(
         'FT_Get_Sfnt_Name_Count',
         FT_UInt,
         (FT_Face,))
 
-FT_Get_Sfnt_Name       = _def_func(
+FT_Get_Sfnt_Name       = _ft_func(
         'FT_Get_Sfnt_Name',
         FT_UInt,
         (FT_Face, FT_UInt, POINTER(FT_SfntName)))
@@ -427,92 +432,92 @@ FT_Get_Sfnt_Name       = _def_func(
 ##### FT_PFR_H #####
 
 ##### FT_STROKER_H #####
-FT_Outline_GetInsideBorder  = _def_func(
+FT_Outline_GetInsideBorder  = _ft_func(
         'FT_Outline_GetInsideBorder',
         FT_StrokerBorder,
         (POINTER(FT_Outline),))
 
-FT_Outline_GetOutsideBorder = _def_func(
+FT_Outline_GetOutsideBorder = _ft_func(
         'FT_Outline_GetOutsideBorder',
         FT_StrokerBorder,
         (POINTER(FT_Outline),))
 
-FT_Stroker_New              = _def_func(
+FT_Stroker_New              = _ft_func(
         'FT_Stroker_New',
         FT_Error,
         (FT_Library, POINTER(FT_Stroker)))
 
-FT_Stroker_Set              = _def_func(
+FT_Stroker_Set              = _ft_func(
         'FT_Stroker_Set',
         None,
         (FT_Stroker, FT_Fixed, FT_Stroker_LineCap, FT_Stroker_LineJoin, FT_Fixed))
 
-FT_Stroker_Rewind           = _def_func(
+FT_Stroker_Rewind           = _ft_func(
         'FT_Stroker_Rewind',
         None,
         (FT_Stroker,) )
 
-FT_Stroker_ParseOutline     = _def_func(
+FT_Stroker_ParseOutline     = _ft_func(
         'FT_Stroker_ParseOutline',
         FT_Error,
         (FT_Stroker, POINTER(FT_Outline), FT_Bool))
 
-FT_Stroker_BeginSubPath     = _def_func(
+FT_Stroker_BeginSubPath     = _ft_func(
         'FT_Stroker_BeginSubPath',
         FT_Error,
         (FT_Stroker, POINTER(FT_Vector), FT_Bool))
 
-FT_Stroker_EndSubPath       = _def_func(
+FT_Stroker_EndSubPath       = _ft_func(
         'FT_Stroker_EndSubPath',
         FT_Error,
         (FT_Stroker,))
 
-FT_Stroker_LineTo           = _def_func(
+FT_Stroker_LineTo           = _ft_func(
         'FT_Stroker_LineTo',
         FT_Error,
         (FT_Stroker, POINTER(FT_Vector)))
 
-FT_Stroker_ConicTo          = _def_func(
+FT_Stroker_ConicTo          = _ft_func(
         'FT_Stroker_ConicTo',
         FT_Error,
         (FT_Stroker, POINTER(FT_Vector), POINTER(FT_Vector)))
 
-FT_Stroker_CubicTo          = _def_func(
+FT_Stroker_CubicTo          = _ft_func(
         'FT_Stroker_CubicTo',
         FT_Error,
         (FT_Stroker, POINTER(FT_Vector), POINTER(FT_Vector), POINTER(FT_Vector)))
 
-FT_Stroker_GetBorderCounts  = _def_func(
+FT_Stroker_GetBorderCounts  = _ft_func(
         'FT_Stroker_GetBorderCounts',
         FT_Error,
         (FT_Stroker, FT_StrokerBorder, POINTER(FT_UInt), POINTER(FT_UInt)))
 
-FT_Stroker_ExportBorder     = _def_func(
+FT_Stroker_ExportBorder     = _ft_func(
         'FT_Stroker_ExportBorder',
         None,
         (FT_Stroker, FT_StrokerBorder, POINTER(FT_Outline)))
 
-FT_Stroker_GetCounts        = _def_func(
+FT_Stroker_GetCounts        = _ft_func(
         'FT_Stroker_GetCounts',
         FT_Error,
         (FT_Stroker, POINTER(FT_UInt), POINTER(FT_UInt)))
 
-FT_Stroker_Export           = _def_func(
+FT_Stroker_Export           = _ft_func(
         'FT_Stroker_Export',
         None,
         (FT_Stroker, POINTER(FT_Outline)))
 
-FT_Stroker_Done             = _def_func(
+FT_Stroker_Done             = _ft_func(
         'FT_Stroker_Done',
         None,
         (FT_Stroker,))
 
-FT_Glyph_Stroke             = _def_func(
+FT_Glyph_Stroke             = _ft_func(
         'FT_Glyph_Stroke',
         FT_Error,
         (POINTER(FT_Glyph), FT_Stroker, FT_Bool))
 
-FT_Glyph_StrokeBorder       = _def_func(
+FT_Glyph_StrokeBorder       = _ft_func(
         'FT_Glyph_StrokeBorder',
         FT_Error,
         (POINTER(FT_Glyph), FT_Stroker, FT_Bool, FT_Bool))
@@ -521,7 +526,7 @@ FT_Glyph_StrokeBorder       = _def_func(
 ##### FT_SYNTHESIS_H #####
 
 ##### FT_XFREE86_H #####
-FT_Get_X11_Font_Format = _def_func(
+FT_Get_X11_Font_Format = _ft_func(
         'FT_Get_X11_Font_Format',
         c_char_p,
         (FT_Face,))
@@ -529,13 +534,13 @@ FT_Get_X11_Font_Format = _def_func(
 ##### FT_TRIGONOMETRY_H #####
 
 ##### FT_LCD_FILTER_H #####
-FT_Library_SetLcdFilter        = _def_func(
+FT_Library_SetLcdFilter        = _ft_func(
         'FT_Library_SetLcdFilter',
         FT_Error,
         (FT_Library, FT_LcdFilter),
         version=(2,3,0))
 
-FT_Library_SetLcdFilterWeights = _def_func(
+FT_Library_SetLcdFilterWeights = _ft_func(
         'FT_Library_SetLcdFilterWeights',
         FT_Error,
         (FT_Library, POINTER(c_ubyte)),
@@ -547,7 +552,7 @@ FT_Library_SetLcdFilterWeights = _def_func(
 ##### FT_GASP_H #####
 
 ##### FT_ADVANCES_H #####
-FT_Get_Advance     = _def_func(
+FT_Get_Advance     = _ft_func(
         'FT_Get_Advance',
         FT_Error,
         (FT_Face, FT_UInt, FT_Int32, POINTER(FT_Fixed)))
