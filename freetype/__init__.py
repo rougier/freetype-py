57b3da9a674a90cbf54e314c7b66272d1f895eb8
Wilhelm Brasch <email@jiong3.org>
2016-11-06 13:35:31 +0100

    Add Face.get_chars() generator.

--- freetype/__init__.py
+++ freetype/__init__.py
@@ -1131,6 +1131,25 @@ class Face( object ):
         if error: raise FT_Exception(error)
         return buff.value
 
+    def get_chars( self ):
+        '''
+        This generator function is used to return all unicode character
+        codes in the current charmap of a given face. For each character it
+        also returns the corresponding glyph index.
+
+        :return: character code, glyph index
+
+        **Note**:
+          Note that 'agindex' is set to 0 if the charmap is empty. The
+          character code itself can be 0 in two cases: if the charmap is empty
+          or if the value 0 is the first valid character code.
+        '''
+        charcode, agindex = self.get_first_char()
+        yield charcode, agindex
+        while agindex != 0:
+            charcode, agindex = self.get_next_char(charcode, 0)
+            yield charcode, agindex
+
     def get_first_char( self ):
         '''
         This function is used to return the first character code in the current
