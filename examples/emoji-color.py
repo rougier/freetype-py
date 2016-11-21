d7f821a24c8a24c0a71f2703cd879e81a99862ce
Christoph Stahl <stahlfabrik@users.noreply.github.com>
2016-11-21 10:35:48 +0100

    Added simple color emoji example
    The example demonstrates rendering an emoji character. In the example
    Apple's MacOS system-emoji-font is used.

---
+++ examples/emoji-color.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import freetype
+import numpy as np
+from PIL import Image
+
+def convert_bgra_to_rgb(buf):
+    blue = buf[:,:,0]
+    green = buf[:,:,1]
+    red = buf[:,:,2]
+    return np.dstack((red, green, blue))
+
+face = freetype.Face("/System/Library/Fonts/Apple Color Emoji.ttc")
+face.set_char_size( 20*64 )
+face.load_char('😀', freetype.FT_LOAD_COLOR)
+bitmap = face.glyph.bitmap
+bitmap = np.array(bitmap.buffer, dtype=np.uint8).reshape((bitmap.rows,bitmap.width,4))
+rgb = convert_bgra_to_rgb(bitmap)
+im = Image.fromarray(rgb)
+im.show()
