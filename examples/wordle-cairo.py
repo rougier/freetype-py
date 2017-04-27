e94090455dfb871581cc3c242a086d725f743389
Hin-Tak Leung <htl10@users.sourceforge.net>
2017-04-27 19:34:08 +0100

    wordle: improving tight-packing check, and allow running under older pycairo
    The worlde example no longer requires pycairo 1.11+ . It now
    packs as tightly as the old (and still faster). The tight-packing check
    depends on the background being black, so that's changed.
    
    Still to investigate: The different tiles should have different alphas.

--- examples/wordle-cairo.py
+++ examples/wordle-cairo.py
@@ -8,17 +8,31 @@
 #  rewrite of the numply,matplotlib-based example from Nicolas P. Rougier
 #  - Cairo can paint partly off-screen, so this example does!
 #
-#  This example needs pycairo 1.11+ (released in 2017-04-09). Auto-checked.
+#  This example behaves differently under pycairo 1.11+ (released in 2017-04-09).
+#  Auto-checked. On older pycairo, it does not draw partial patterns at the edges.
 #
 # -----------------------------------------------------------------------------
 from math import cos, sin
-from numpy import random, sort, sqrt
+from numpy import random, sort, sqrt, ndarray, ubyte
 from freetype import *
 
 try:
+    # pycairo 1.11+:
     from cairo import Region, RectangleInt, REGION_OVERLAP_OUT
 except ImportError:
-    raise RuntimeError('Region class not found; Need pycairo 1.11+')
+    # stubs for pycairo < 1.11:
+    class Region:
+        def __init__(self):
+            return
+        def union(self, rec):
+            return
+        def contains_rectangle(self, rec):
+            # inhibit drawing
+            return True
+    class RectangleInt:
+        def __init__(self, x, y, w, h):
+            return
+    REGION_OVERLAP_OUT = False
 
 from cairo import Context, ImageSurface, FORMAT_A8, FORMAT_ARGB32, Matrix
 from bitmap_to_surface import make_image_surface
@@ -101,12 +115,12 @@ def make_label(text, filename, size=12, angle=0):
 if __name__ == '__main__':
     from PIL import Image
 
-    n_words = 140
+    n_words = 200
     H, W, dpi = 600, 800, 72.0
-    I = ImageSurface(FORMAT_ARGB32, W, H)
+    I = ImageSurface(FORMAT_A8, W, H)
     ctxI = Context(I)
     ctxI.rectangle(0,0,800,600)
-    ctxI.set_source_rgb (0.9, 0.9, 0.9)
+    ctxI.set_source_rgba (0.9, 0.9, 0.9, 0)
     ctxI.fill()
     S = random.normal(0,1,n_words)
     S = (S-S.min())/(S.max()-S.min())
@@ -135,8 +149,18 @@ if __name__ == '__main__':
                 c = .25+.75*random.random()
                 x = int(x0+dx)
                 y = int(y0+dy)
+                checked = False
+                I.flush()
+                if not (x <= w//2 or y <= h//2 or x >= (W-w//2) or y >= (H-h//2)):
+                    ndI = ndarray(shape=(h,w), buffer=I.get_data(), dtype=ubyte, order='C',
+                                  offset=(x-w//2) + I.get_stride() * (y-h//2),
+                                  strides=[I.get_stride(), 1])
+                    ndL = ndarray(shape=(h,w), buffer=L.get_data(), dtype=ubyte, order='C',
+                                  strides=[L.get_stride(), 1])
+                    if ((ndI * ndL).sum() == 0):
+                       checked = True
                 new_region = RectangleInt(x-w//2, y-h//2, w, h)
-                if ( drawn_regions.contains_rectangle(new_region) == REGION_OVERLAP_OUT ):
+                if  (checked or ( drawn_regions.contains_rectangle(new_region) == REGION_OVERLAP_OUT )):
                     ctxI.set_source_surface(L, 0, 0)
                     pattern = ctxI.get_source()
                     scalematrix = Matrix()
@@ -144,7 +168,7 @@ if __name__ == '__main__':
                     scalematrix.translate(w//2 - x, h//2 - y)
                     pattern.set_matrix(scalematrix)
                     ctxI.mask(pattern)
-                    ctxI.set_source_rgb(c,0,c)
+                    ctxI.set_source_rgba(c,c,c,c)
                     ctxI.fill()
                     drawn_regions.union(new_region)
                     break
