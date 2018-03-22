16c73db9add01a241e56934fa6a5af06a1aba86c
Nikolaus Waxweiler <madigens@gmail.com>
2018-03-22 18:57:01 +0000

    Test version when bundling FT

--- tests/tests/test_ft_face.py
+++ tests/tests/test_ft_face.py
@@ -1,6 +1,11 @@
+import os
+
 import freetype
 
 
 def test_ft_face():
     """A smoke test."""
     assert freetype.Face('../examples/Vera.ttf')
+
+    if os.environ.get("FREETYPEPY_BUNDLE_FT"):
+        assert freetype.version() == (2, 9, 0)
