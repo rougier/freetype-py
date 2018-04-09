1787f1fa204a0456eca08236ee5ecb90b75b0705
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2018-04-09 10:50:59 +0100

    Comment out pre_build()

--- ci/multibuild_config.sh
+++ ci/multibuild_config.sh
@@ -1,12 +1,11 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
-function pre_build {
-    # Any stuff that you need to do before you start building the wheels
-    # Runs in the root directory of this repository.
-
-    # ...
-}
+# function pre_build {
+#     # Any stuff that you need to do before you start building the wheels
+#     # Runs in the root directory of this repository.
+#
+# }
 
 function run_tests {
     # The function is called from an empty temporary directory.
