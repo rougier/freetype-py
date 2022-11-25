fa96e5734df09f1cd27642b90aa6ccbcde840051
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 06:11:00 +0000

    Adding tarfile member sanitization to extractall()

--- setup-build-freetype.py
+++ setup-build-freetype.py
@@ -147,7 +147,26 @@ def ensure_downloaded(url, sha256_sum):
         assert hasher.hexdigest() == sha256_sum
 
         with tarfile.open(tarball, "r:xz") as tb:
-            tb.extractall(build_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tb, build_dir)
 
 
 distutils.dir_util.mkpath(build_dir)
