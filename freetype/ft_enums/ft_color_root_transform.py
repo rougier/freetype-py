10072e3fb10b84e7b13d6ddc9f38f079f11f3bac
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-07-15 23:18:05 +0100

    Adding FT_Color_Root_Transform enum

---
+++ freetype/ft_enums/ft_color_root_transform.py
@@ -0,0 +1,6 @@
+FT_Color_Root_Transform = {
+    'FT_COLOR_INCLUDE_ROOT_TRANSFORM' : 0,
+    'FT_COLOR_NO_ROOT_TRANSFORM'      : 1,
+    'FT_COLOR_ROOT_TRANSFORM_MAX'     : 2,
+}
+globals().update(FT_Color_Root_Transform)
