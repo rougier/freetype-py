b0e753276285f8ea379cb16bdf9c15974a92ac17
Cosimo Lupo <clupo@google.com>
2024-03-08 12:42:21 +0000

    use custom PEP517 build backend to require cmake conditionally
    Fixes https://github.com/rougier/freetype-py/issues/183
    
    `pip install --no-binary freetype-py freetype-py` will no longer attempt to download and install cmake in pip's temporary build environment, unless FREETYPEPY_BUNDLE_FT environment variable was set in order to compile the bundled FreeType shared library.
    
    This makes the installation of a pure Python freetype-py (using system-wide FreeType library) quicker and doesn't require one to force --no-build-isolation just to prevent cmake from being installed unnecessarily.

---
+++ _custom_build/backend.py
@@ -0,0 +1,25 @@
+"""Custom PEP 517 build backend to provide different dependencies for wheel builds.
+
+We need some extra dependencies when we build a wheel that bundles the FreeType
+shared library (triggered when FREETYPEPY_BUNDLE_FT environment variable is set),
+as opposed to a pure Python wheel (py3-none-any.whl).
+
+For more info see:
+
+https://setuptools.pypa.io/en/latest/build_meta.html#dynamic-build-dependencies-and-other-build-meta-tweaks
+
+https://github.com/rougier/freetype-py/issues/183
+"""
+
+import os
+from setuptools import build_meta as _orig
+from setuptools.build_meta import *
+
+
+def get_requires_for_build_wheel(config_settings=None):
+    build_wheel_deps = _orig.get_requires_for_build_wheel(config_settings)
+
+    if os.environ.get("FREETYPEPY_BUNDLE_FT"):
+        build_wheel_deps += ["certifi", "cmake"]
+
+    return build_wheel_deps
